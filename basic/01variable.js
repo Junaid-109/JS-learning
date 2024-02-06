7dc8c6f83da7c8666d0cd7b05f19e2e00336dd71
Junaid-109 <141545875+Junaid-109@users.noreply.github.com>
2024-02-06 07:32:59 +0000

    2nd

---
+++ basic/01variable.js
@@ -0,0 +1,12 @@
+const accountID = 114413
+let accountEmail = "abc123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState
+
+//accountID = "2" not allowed
+
+accountEmail = "321bcd@gmail"
+accountPassword = "213141"
+accountCity = "lahore"
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
